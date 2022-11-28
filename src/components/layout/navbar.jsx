d520d0e2d3934169e422eb6a81cbf7a8003c06cf
Jordan Duran <jordanduran@Jordans-MacBook-Pro.local>
2022-11-27 20:14:48 -0500

    Project revert -> Twinnleaf

--- src/components/layout/navbar.jsx
+++ src/components/layout/navbar.jsx
@@ -1,171 +1,122 @@
-import { Fragment } from 'react';
-import { Disclosure, Menu, Transition } from '@headlessui/react';
-import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline';
-import { Link } from 'react-router-dom';
+/* This example requires Tailwind CSS v3.0+ */
+import { useState } from 'react';
+import { Dialog } from '@headlessui/react';
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline';
 
-function classNames(...classes) {
-  return classes.filter(Boolean).join(' ');
-}
+const navigation = [
+  { name: 'Flower', href: '#' },
+  { name: 'CBD', href: '#' },
+  { name: 'Gear', href: '#' },
+];
 
 const Navbar = () => {
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
   return (
-    <Disclosure as='nav' className='bg-white shadow'>
-      {({ open }) => (
-        <>
-          <div className='mx-auto max-w-7xl px-2 sm:px-6 lg:px-8'>
-            <div className='relative flex h-16 justify-between'>
-              <div className='absolute inset-y-0 left-0 flex items-center sm:hidden'>
-                {/* Mobile menu button */}
-                <Disclosure.Button className='inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-yellow-400'>
-                  <span className='sr-only'>Open main menu</span>
-                  {open ? (
-                    <XMarkIcon className='block h-6 w-6' aria-hidden='true' />
-                  ) : (
-                    <Bars3Icon className='block h-6 w-6' aria-hidden='true' />
-                  )}
-                </Disclosure.Button>
-              </div>
-              <div className='flex flex-1 items-center justify-center sm:items-stretch sm:justify-start'>
-                <div className='flex flex-shrink-0 items-center'>
+    <div className='px-6 pt-6 lg:px-8 mb-6'>
+      <div>
+        <nav
+          className='flex h-9 items-center justify-between'
+          aria-label='Global'
+        >
+          <div className='flex lg:min-w-0 lg:flex-1' aria-label='Global'>
+            <a href='#' className='-m-1.5 p-1.5'>
+              <span className='sr-only'>Your Company</span>
+              <img
+                className='block h-12 w-auto lg:hidden'
+                src={require('../../images/twinnleaf-logo.png')}
+                alt='Your Company'
+              />
+              <img
+                className='hidden h-14 w-auto lg:block'
+                src={require('../../images/twinnleaf-logo.png')}
+                alt='Your Company'
+              />
+            </a>
+          </div>
+          <div className='flex lg:hidden'>
+            <button
+              type='button'
+              className='-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700'
+              onClick={() => setMobileMenuOpen(true)}
+            >
+              <span className='sr-only'>Open main menu</span>
+              <Bars3Icon className='h-6 w-6' aria-hidden='true' />
+            </button>
+          </div>
+          <div className='hidden lg:flex lg:min-w-0 lg:flex-1 lg:justify-center lg:gap-x-12'>
+            {navigation.map((item) => (
+              <a
+                key={item.name}
+                href={item.href}
+                className='font-semibold text-gray-900 hover:text-gray-900'
+              >
+                {item.name}
+              </a>
+            ))}
+          </div>
+          <div className='hidden lg:flex lg:min-w-0 lg:flex-1 lg:justify-end'>
+            <a
+              href='#'
+              className='inline-block rounded-lg px-3 py-1.5 text-sm font-semibold leading-6 text-gray-900 shadow-sm ring-1 ring-gray-900/10 hover:ring-gray-900/20'
+            >
+              Log in
+            </a>
+          </div>
+        </nav>
+        <Dialog as='div' open={mobileMenuOpen} onClose={setMobileMenuOpen}>
+          <Dialog.Panel
+            focus='true'
+            className='fixed inset-0 z-10 overflow-y-auto bg-white px-6 py-6 lg:hidden'
+          >
+            <div className='flex h-9 items-center justify-between'>
+              <div className='flex'>
+                <a href='#' className='-m-1.5 p-1.5'>
+                  <span className='sr-only'>Your Company</span>
                   <img
-                    className='block h-12 w-auto lg:hidden'
-                    src={require('../../images/blockbuster-logo.png')}
-                    alt='Your Company'
+                    className='h-8'
+                    src={require('../../images/twinnleaf-logo.png')}
+                    alt=''
                   />
-                  <img
-                    className='hidden h-10 w-auto lg:block'
-                    src={require('../../images/blockbuster-logo.png')}
-                    alt='Your Company'
-                  />
-                </div>
-                <div className='hidden sm:ml-6 sm:flex sm:space-x-8'>
-                  {/* Current: "border-yellow-400 text-gray-900", Default: "border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700" */}
-                  <Link
-                    to='#'
-                    className='inline-flex items-center border-b-2 border-yellow-400 px-1 pt-1 text-sm font-medium text-gray-900'
-                  >
-                    Movies
-                  </Link>
-                  <Link
-                    to='#'
-                    className='inline-flex items-center border-b-2 border-transparent px-1 pt-1 text-sm font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700'
-                  >
-                    Series
-                  </Link>
-                  <Link
-                    to='#'
-                    className='inline-flex items-center border-b-2 border-transparent px-1 pt-1 text-sm font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700'
-                  >
-                    Trending
-                  </Link>
-                </div>
+                </a>
               </div>
-              <div className='absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0'>
+              <div className='flex'>
                 <button
                   type='button'
-                  className='rounded-full bg-white p-1 text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-yellow-400 focus:ring-offset-2'
+                  className='-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700'
+                  onClick={() => setMobileMenuOpen(false)}
                 >
-                  <span className='sr-only'>View notifications</span>
-                  <BellIcon className='h-6 w-6' aria-hidden='true' />
+                  <span className='sr-only'>Close menu</span>
+                  <XMarkIcon className='h-6 w-6' aria-hidden='true' />
                 </button>
-
-                {/* Profile dropdown */}
-                <Menu as='div' className='relative ml-3'>
-                  <div>
-                    <Menu.Button className='flex rounded-full bg-white text-sm focus:outline-none focus:ring-2 focus:ring-yellow-400 focus:ring-offset-2'>
-                      <span className='sr-only'>Open user menu</span>
-                      <img
-                        className='h-8 w-8 rounded-full'
-                        src='https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80'
-                        alt=''
-                      />
-                    </Menu.Button>
-                  </div>
-                  <Transition
-                    as={Fragment}
-                    enter='transition ease-out duration-200'
-                    enterFrom='transform opacity-0 scale-95'
-                    enterTo='transform opacity-100 scale-100'
-                    leave='transition ease-in duration-75'
-                    leaveFrom='transform opacity-100 scale-100'
-                    leaveTo='transform opacity-0 scale-95'
-                  >
-                    <Menu.Items className='absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none'>
-                      <Menu.Item>
-                        {({ active }) => (
-                          <Link
-                            to='#'
-                            className={classNames(
-                              active ? 'bg-gray-100' : '',
-                              'block px-4 py-2 text-sm text-gray-700'
-                            )}
-                          >
-                            Your Profile
-                          </Link>
-                        )}
-                      </Menu.Item>
-                      <Menu.Item>
-                        {({ active }) => (
-                          <Link
-                            to='#'
-                            className={classNames(
-                              active ? 'bg-gray-100' : '',
-                              'block px-4 py-2 text-sm text-gray-700'
-                            )}
-                          >
-                            Settings
-                          </Link>
-                        )}
-                      </Menu.Item>
-                      <Menu.Item>
-                        {({ active }) => (
-                          <Link
-                            to='#'
-                            className={classNames(
-                              active ? 'bg-gray-100' : '',
-                              'block px-4 py-2 text-sm text-gray-700'
-                            )}
-                          >
-                            Sign out
-                          </Link>
-                        )}
-                      </Menu.Item>
-                    </Menu.Items>
-                  </Transition>
-                </Menu>
               </div>
             </div>
-          </div>
-
-          <Disclosure.Panel className='sm:hidden'>
-            <div className='space-y-1 pt-2 pb-4'>
-              {/* Current: "bg-yellow-50 border-yellow-400 text-yellow-700", Default: "border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700" */}
-              <Disclosure.Button
-                as='a'
-                href='#'
-                className='block border-l-4 border-yellow-400 bg-yellow-50 py-2 pl-3 pr-4 text-base font-medium text-yellow-700'
-              >
-                Movies
-              </Disclosure.Button>
-              <Disclosure.Button
-                as='a'
-                href='#'
-                className='block border-l-4 border-transparent py-2 pl-3 pr-4 text-base font-medium text-gray-500 hover:border-gray-300 hover:bg-gray-50 hover:text-gray-700'
-              >
-                Series
-              </Disclosure.Button>
-              <Disclosure.Button
-                as='a'
-                href='#'
-                className='block border-l-4 border-transparent py-2 pl-3 pr-4 text-base font-medium text-gray-500 hover:border-gray-300 hover:bg-gray-50 hover:text-gray-700'
-              >
-                Trending
-              </Disclosure.Button>
+            <div className='mt-6 flow-root'>
+              <div className='-my-6 divide-y divide-gray-500/10'>
+                <div className='space-y-2 py-6'>
+                  {navigation.map((item) => (
+                    <a
+                      key={item.name}
+                      href={item.href}
+                      className='-mx-3 block rounded-lg py-2 px-3 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-400/10'
+                    >
+                      {item.name}
+                    </a>
+                  ))}
+                </div>
+                <div className='py-6'>
+                  <a
+                    href='#'
+                    className='-mx-3 block rounded-lg py-2.5 px-3 text-base font-semibold leading-6 text-gray-900 hover:bg-gray-400/10'
+                  >
+                    Log in
+                  </a>
+                </div>
+              </div>
             </div>
-          </Disclosure.Panel>
-        </>
-      )}
-    </Disclosure>
+          </Dialog.Panel>
+        </Dialog>
+      </div>
+    </div>
   );
 };
 
